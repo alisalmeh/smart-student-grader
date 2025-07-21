366408bf1f50a9fd918c62d6d1b9fcc1f124e98f
Ali Salmeh <alisalmeh18@gmail.com>
2025-07-21 19:26:42 +0330

    feat: add generator module

---
+++ grader/generator.py
@@ -0,0 +1,29 @@
+def get_student_data():
+    students = {}
+  
+    print("\nEnter student names and scores.")
+
+    while True:
+        name = input("Enter student name (or type 'done' to finish): ").strip()
+        if name.lower() == 'done':
+            break
+
+        try:
+            scores_input = input("Enter 3 scores separated by space (e.g. 38 75 91): ")
+            scores = list(map(int, scores_input.strip().split()))
+
+            if len(scores) != 3:
+                print("Please enter exactly 3 scores")
+                continue
+            elif any(score<0 or score>100 for score in scores):
+                print("Scores must be between 0 and 100")
+                continue
+                            
+            students[name] = scores
+
+        except ValueError:
+            print("Invalid input. Only number!")
+
+    return students
+
+print(get_student_data())
